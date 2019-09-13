06b2275642d5a4de03f0317820ef4f04c9d4b7e3
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-09-13 23:44:25 +0200

    Added header active with php

--- views/modules/header.php
+++ views/modules/header.php
@@ -3,10 +3,10 @@
         <nav class="c-header-nav">
             <ul class="c-header-nav__list c-list__no">
                 <li class="c-header-nav__item">
-                    <a class="c-header-nav__link" href="index.php?action=calendar-1">Calendar 1</a>
+                    <a class="c-header-nav__link <?php if($_GET['action'] == "calendar-1") { ?>is-active<?php } ?>" href="index.php?action=calendar-1">Calendar 1</a>
                 </li>
                 <li class="c-header-nav__item">
-                    <a class="c-header-nav__link" href="index.php?action=calendar-2">Calendar 2</a>
+                    <a class="c-header-nav__link <?php if($_GET['action'] == "calendar-2") {?>is-active<?php } ?>" href="index.php?action=calendar-2">Calendar 2</a>
                 </li>
             </ul>
         </nav>
