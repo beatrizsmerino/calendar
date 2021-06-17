e9245198a872642c5315b616c54999b73396b820
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-06-17 17:59:11 +0200

    REFACTORING. Changed 'var' to 'let' and 'let' to 'const'

--- src/js/pages/_pages-calendar-1.js
+++ src/js/pages/_pages-calendar-1.js
@@ -92,102 +92,105 @@ const settings = {
 };
 
 function createYearMonthDay(year, month, day) {
-	let yyyy = String(year);
-	let mm = String(month + 1).padStart(2, "0"); //January is 0!
-	let dd = String(day).padStart(2, "0");
-
-	let yearMonthDay = yyyy + "-" + mm + "-" + dd;
+	const yyyy = String(year);
+	const mm = String(month + 1).padStart(2, "0"); //January is 0!
+	const dd = String(day).padStart(2, "0");
+	const yearMonthDay = yyyy + "-" + mm + "-" + dd;
 	return yearMonthDay;
 }
 
 function getThisYear() {
-	let date = new Date();
+	const date = new Date();
 	return date.getFullYear();
 }
 
 function getThisMonth() {
-	let date = new Date();
+	const date = new Date();
 	return date.getMonth();
 }
 
 function getThisDay() {
-	let date = new Date();
+	const date = new Date();
 	return date.getDate();
 }
 
 function getDayOfYear(year, day) {
-	let dateDay = new Date(year, 0);
+	const dateDay = new Date(year, 0);
 	return new Date(dateDay.setDate(day));
 }
 
 function getToday() {
-	let today = createYearMonthDay(getThisYear(), getThisMonth(), getThisDay());
+	const today = createYearMonthDay(
+		getThisYear(),
+		getThisMonth(),
+		getThisDay()
+	);
 	return today;
 }
 
 function get4Letters(words) {
-	let wordsFormatted = words.map((item) => item.slice(0, 4));
+	const wordsFormatted = words.map((item) => item.slice(0, 4));
 
 	return wordsFormatted;
 }
 
 function calendarCreateStructure(monthsList, weeksList) {
 	// YEAR
-	let year = document.createElement("div");
+	const year = document.createElement("div");
 	year.className = "calendar__year";
 	year.innerText = getThisYear();
 	document.getElementById("calendar").appendChild(year);
 
 	// INNER
-	let calendarInner = document.createElement("div");
+	const calendarInner = document.createElement("div");
 	calendarInner.className = "calendar__inner";
 	document.getElementById("calendar").appendChild(calendarInner);
 
 	for (let m = 0; m <= 11; m++) {
 		// MONTHS
-		let month = document.createElement("DIV");
+		const month = document.createElement("DIV");
 		month.className = "calendar__month";
 		calendarInner.appendChild(month);
 
 		// TABLE
-		let tableMonth = document.createElement("TABLE");
+		const tableMonth = document.createElement("TABLE");
 		tableMonth.className = "calendar__table";
 		month.appendChild(tableMonth);
 
 		// TABLE - MONTHS
-		let monthTitle = document.createElement("CAPTION");
+		const monthTitle = document.createElement("CAPTION");
 		monthTitle.className = "calendar__title";
 		monthTitle.innerText = monthsList[m];
 		tableMonth.appendChild(monthTitle);
 
 		// TABLE HEADER - WEEKS
-		let tableHeader = document.createElement("THEAD");
+		const tableHeader = document.createElement("THEAD");
 		tableHeader.className = "calendar__header";
 		tableMonth.appendChild(tableHeader);
 
-		let tableRow = document.createElement("TR");
+		const tableRow = document.createElement("TR");
 		tableRow.className = "calendar__row";
 		tableHeader.appendChild(tableRow);
 
 		for (let w = 0; w < 7; w++) {
-			let tableWeek = document.createElement("TH");
+			const tableWeek = document.createElement("TH");
 			tableWeek.className = "calendar__cell calendar__week";
 			tableWeek.innerText = weeksList[w];
 			tableRow.appendChild(tableWeek);
 		}
 
 		// TABLE BODY - DAYS
-		let tableBody = document.createElement("TBODY");
+		const tableBody = document.createElement("TBODY");
 		tableBody.className = "calendar__body";
 		tableMonth.appendChild(tableBody);
 
 		for (let f = 0; f < 6; f++) {
-			let tableRow = document.createElement("TR");
+			const tableRow = document.createElement("TR");
 			tableRow.className = "calendar__row";
 			tableBody.appendChild(tableRow);
 
 			for (let d = 0; d < 7; d++) {
-				let tableDays = document.createElement("TD");
+				const tableDays = document.createElement("TD");
 				tableDays.className = "calendar__cell calendar__day";
 				tableDays.innerText = "";
 				tableRow.appendChild(tableDays);
@@ -197,27 +200,27 @@ function calendarCreateStructure(monthsList, weeksList) {
 }
 
 function calendarSetDays() {
+	const thisYear = getThisYear();
 	let week = 0;
-	let thisYear = getThisYear();
 
 	for (let i = 1; i < 366; i++) {
-		let dayOfYear = getDayOfYear(thisYear, i);
-		let dateDay = dayOfYear.getDate();
-		let dateMonth = dayOfYear.getMonth();
-		let dateWeek = dayOfYear.getDay();
-		let calendarMonth =
-			document.getElementsByClassName("calendar__table")[dateMonth];
+		const dayOfYear = getDayOfYear(thisYear, i);
+		const dateDay = dayOfYear.getDate();
+		const dateMonth = dayOfYear.getMonth();
+		const dateWeek = dayOfYear.getDay();
+		const calendarMonth =
+			document.querySelectorAll(".calendar__table")[dateMonth];
 
 		if (dateDay == 1) {
 			week = 0;
 		}
 
 		// insert days in the calendar
-		let tableDays =
+		const tableDays =
 			calendarMonth.children[2].children[week].children[dateWeek];
 		tableDays.innerHTML = `<span>${dateDay}</span>`;
 
-		let yearMonthDay = createYearMonthDay(thisYear, dateMonth, dateDay);
+		const yearMonthDay = createYearMonthDay(thisYear, dateMonth, dateDay);
 		tableDays.setAttribute("data-time", yearMonthDay);
 
 		if (tableDays.getAttribute("data-time") == getToday()) {
@@ -243,11 +246,11 @@ function calendarResetWidth() {
 }
 
 function calendarSetWeekend() {
-	let weekendSaturdays = document.querySelectorAll(
+	const weekendSaturdays = document.querySelectorAll(
 		".calendar__body .calendar__row .calendar__cell:last-child"
 	);
 
-	let weekendSundays = document.querySelectorAll(
+	const weekendSundays = document.querySelectorAll(
 		".calendar__body .calendar__row .calendar__cell:first-child"
 	);
 
@@ -265,7 +268,7 @@ function calendarMoveScrollToday() {
 
 	let positionScroll = 0;
 	for (let index = 0; index < currentMonth; index++) {
-		let style =
+		const style =
 			calendarMonth[index].currentStyle ||
 			window.getComputedStyle(calendarMonth[index]);
 		positionScroll +=
@@ -301,7 +304,7 @@ function calendarTranslate() {
 
 	if (document.querySelectorAll("#selectTranslate option").length === 1) {
 		settings.languages.map((item) => {
-			let option = document.createElement("option");
+			const option = document.createElement("option");
 			option.value = item.value;
 			option.innerText = item.text;
 			select.appendChild(option);
