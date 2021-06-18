808c6bfde597c72d1cb39a505a971653edde7c5c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-06-18 02:51:45 +0200

    SPRITES. Changed url of 'sprites.svg'
    - BUG FIXED. Wrong fetch on github pages

--- src/js/components/_components-sprite.js
+++ src/js/components/_components-sprite.js
@@ -10,7 +10,7 @@
  * @description Insert file sprites.svg with the icons to the end of the all html files
  */
 function printSprites() {
-	const url = './../images/icons/sprites.svg';
+	const url = 'images/icons/sprites.svg';
 	const className = 'sprite';
 
 	const getContentFile = async(urlFile) => {
