2c3e5a8c4aabffbcf31336673122da49b8953314
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-07-08 00:35:04 +0200

    CALENDAR 3. Created 'calendar-3' page
    - Added sass, js and html files
    - Commented temporary empty selectors of sass
    - Added description of the 'calendar-3' on 'Home' page
    - Created 'ellipsis' and link components and use it on 'Home' page

---
+++ src/js/pages/_pages-calendar-3.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+	const pageCalendar2 = document
+		.querySelector("html")
+		.classList.contains("page-calendar-3");
+
+	if (pageCalendar2) {
+		console.log("Calendar 3");
+	}
+});
