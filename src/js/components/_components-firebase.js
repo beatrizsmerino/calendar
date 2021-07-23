bfada9fd200d5fdb45d4134b09229a75fdb9d318
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-07-23 23:32:10 +0200

    FIREBASE. File created to build the Firebase code for Authentication and Realtime Database

---
+++ src/js/components/_components-firebase.js
@@ -0,0 +1,11 @@
+/**
+ * @file componentFirebase
+ * @description Authentication and CRUD (Create, Read, Update and Delete)
+ * @author Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
+ * @copyright (2021)
+ */
+
+
+
+
+
