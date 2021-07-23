7ddb8ee3f3edd4b7bc42807d768b03aac04fa8e7
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-07-23 23:18:38 +0200

    BUG FIXED. Wrong variable name

--- src/js/pages/_pages-calendar-3.js
+++ src/js/pages/_pages-calendar-3.js
@@ -1,11 +1,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-	const pageCalendar2 = document
+	const pageCalendar3 = document
 		.querySelector("html")
 		.classList.contains("page-calendar-3");
 
-	if (pageCalendar2) {
+	if (pageCalendar3) {
 		const settings = {
 			months: [
 				"Enero",
