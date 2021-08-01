fe8a15165711272911c79ce8d01b4755f7dc7113
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-08-01 12:56:46 +0200

    REFACTORING. Renamed, ordered and created functions and variables (Single Responsibility Principle) on 'Modal' component build with js

--- src/js/modules/components/_components-modal.js
+++ src/js/modules/components/_components-modal.js
@@ -20,11 +20,11 @@ import * as tools from '../components/_components-tools.js';
 
 /**
  * @function createTemplate
- * @description Create template of modal al insert on the html
- * @param {String} content 
+ * @description Create template of modal
+ * @param {Object} data Data for build the template
  * @returns 
  */
-function createTemplate(content) {
+function createTemplate(data) {
 	const template = `
 		<div class="modal">
 			<div class="modal__box">
@@ -37,47 +37,102 @@ function createTemplate(content) {
 						</i>
 					</button>
 					<div class="modal__content">
-						${content}
+						${data}
 					</div>
 				</div>
 			</div>
 		</div>
 	`;
 
-	const templateNode = tools.stringToNode(template);
-	document.querySelector('body').appendChild(templateNode);
+	return template;
+}
+
+
+
+/**
+ * @function create
+ * @description Insert the template of modal into the html
+ * @param {Object} data Data for build the template
+ */
+function create(data) {
+	const template = createTemplate(data);
+	const nodeTemplate = tools.stringToNode(template);
+
+	document.querySelector("body").appendChild(nodeTemplate);
+}
+
+
+
+/**
+ * @function remove
+ * @description Remove the modal
+ */
+function remove() {
+	const modal = document.querySelector('.modal');
+
+	if (modal) {
+		modal.remove();
+	}
+}
+
+
+
+/**
+ * @function show
+ * @description Show the modal
+ * - Add the class 'is-show'
+ * @param {Number} time Time to wait before show the message
+ */
+function show(time) {
+	const modal = document.querySelector('.modal');
+
+	setTimeout(function () {
+		modal.classList.add('is-show');
+	}, time);
+}
+
+
+
+/**
+ * @function hide
+ * @description Hide the modal
+ * - Remove the class 'is-show'
+ * - Remove the html
+ * @param {Number} time Time to wait before remove the message
+ */
+function hide(time) {
+	const modal = document.querySelector('.modal');
+
+	modal.classList.remove('is-show');
+
+	setTimeout(function () {
+		remove();
+	}, time);
 }
 
 
 
 /**
  * @function open
- * @description Show modal adding a class
+ * @description Show modal when click on the open button
+ * - Show after a 0.1s
  */
 function open() {
-	const modal = document.querySelector('.modal');
-
-	setTimeout(function () {
-		modal.classList.add('is-show');
-	}, 100);
+	show(100);
 }
 
 
 
 /**
  * @function close
- * @description Hide modal removing a class and his html
+ * @description Hide the modal when click on the close button
+ * - Hide after a 1s
  */
 function close() {
-	const modal = document.querySelector('.modal');
 	const buttonClose = document.querySelector('.modal__button-close');
 
 	buttonClose.addEventListener('click', function () {
-		modal.classList.remove('is-show');
-
-		setTimeout(function () {
-			modal.remove();
-		}, 1000);
+		hide(1000);
 	});
 }
 
@@ -97,10 +152,11 @@ function addEvents() {
 /**
  * @function init
  * @description Create modal with the content and add events
- * @param {String} content - Content of modal
+ * @param {Object} data Data for build the template
  */
-function init(content) {
-	createTemplate(content);
+function init(data) {
+	remove();
+	create(data);
 	addEvents();
 }
 
